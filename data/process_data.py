e0fef3b7b0f01ad9e7828b9f6ea844cdc3478ce2
Chris <c.rausch020@gmail.com>
2019-11-23 15:33:54 +0000

    ETL process_data script finished, functions load_data, clean_data, save_data implemented

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,63 @@
+# import libraries
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    
+    # merge datasets
+    df = messages.merge(categories,how='outer',on=['id'])
+    
+    # create a dataframe of the 36 individual category columns
+    categories = categories['categories'].str.split(';',expand=True)
+                                                    
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = list(row.apply(lambda category: category[:-2]))
+                                                    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+         
+    # Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+    
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+                                                    
+    # drop the original categories column from `df`
+    df=df.drop(columns=['categories'])
+                                                    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+
+    return df
 
 
 def clean_data(df):
-    pass
+    # drop duplicates
+    df = df.drop_duplicates()
+                                                    
+    # drop missing values
+    df = df.dropna()
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
